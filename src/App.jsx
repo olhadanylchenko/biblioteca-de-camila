40b6ad38cf523be7b07b7c5233638a1275f6c700
Olha Danylchenko <olhadanylchenko@air-olha.localdomain>
2020-01-15 11:35:42 +0100

    restore functionality from original project

---
+++ src/App.jsx
@@ -0,0 +1,224 @@
+import React from "react";
+import "./App.css";
+import edit from "./edit.png";
+import search from "./search.png";
+import del from "./del.png";
+import check from "./check.png";
+import db from "localforage";
+import BookList from "./BookList";
+
+class App extends React.Component {
+  state = {
+    books: [],
+    edit: -1,
+    search: ""
+  };
+  constructor(props) {
+    super(props);
+
+    db.getItem("books").then(books => {
+      if (books) {
+        this.setState({ books });
+      } else {
+        db.setItem("books", this.state.books);
+      }
+    });
+  }
+
+  authorRef = React.createRef();
+  titleRef = React.createRef();
+  formRef = React.createRef();
+
+  onSubmit = e => {
+    e.preventDefault();
+    const books = [...this.state.books];
+    const book = {
+      id: books[books.length - 1] ? books[books.length - 1].id + 1 : 1,
+      author: this.authorRef.current.value,
+      title: this.titleRef.current.value,
+      finished: false
+    };
+    books.push(book);
+    this.setState({
+      books
+    });
+    db.setItem("books", books);
+    this.formRef.current.reset();
+  };
+
+  onDelete = id => () => {
+    const books = this.state.books.filter(book => book.id !== id);
+    this.setState({
+      books,
+      edit: -1
+    });
+    db.setItem("books", books);
+  };
+
+  onCheck = index => () => {
+    const book = {
+      ...this.state.books[index]
+    };
+    book.finished = book.finished ? false : true;
+    this.toggleBookFinished(index, book);
+  };
+
+  toggleBookFinished = (index, book) => {
+    const books = [...this.state.books];
+    books[index] = book;
+    this.setState({
+      books
+    });
+    db.setItem("books", books);
+  };
+
+  onClickEdit = index => event => {
+    if (this.state.edit !== index) {
+      this.setState({
+        edit: index
+      });
+    } else {
+      this.setState({
+        edit: -1
+      });
+    }
+  };
+
+  onChange = e => {
+    const books = [...this.state.books];
+    const book = {
+      ...this.state.books[this.state.edit],
+      [e.currentTarget.name]: e.currentTarget.value
+    };
+    books.splice(this.state.edit, 1, book);
+    this.setState({
+      books
+    });
+  };
+
+  searchOnChange = e => {
+    this.setState({
+      search: e.target.value
+    });
+  };
+
+  handleSort = smth => {
+    const books = [...this.state.books];
+    books.sort(function(a, b) {
+      var x = a.author.toLowerCase();
+      var y = b.author.toLowerCase();
+      return x < y ? -1 : x > y ? 1 : 0;
+    });
+    this.setState({
+      books
+    });
+  };
+
+  handleSortTitle = smth => {
+    const books = [...this.state.books];
+    books.sort(function(a, b) {
+      var x = a.title.toLowerCase();
+      var y = b.title.toLowerCase();
+      return x < y ? -1 : x > y ? 1 : 0;
+    });
+    this.setState({
+      books
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header></header>
+        <h1>Biblioteca de Camila</h1>
+
+        <form onSubmit={this.onSubmit} ref={this.formRef}>
+          <table>
+            <tbody>
+              <tr className="addingBooks">
+                <td></td>
+                <td></td>
+                <td></td>
+                <td colSpan="3" className="searchTd">
+                  <input
+                    id="search"
+                    type="text"
+                    className="search"
+                    onChange={this.searchOnChange}
+                  />
+                  <label htmlFor="search" className="searchLabel">
+                    <img src={search} alt="search" height="16" />
+                  </label>
+                </td>
+              </tr>
+
+              <tr className="addingBooks">
+                <td></td>
+                <td>
+                  <input
+                    type="text"
+                    placeholder="author"
+                    ref={this.authorRef}
+                  />
+                </td>
+                <td>
+                  <input type="text" placeholder="title" ref={this.titleRef} />
+                </td>
+                <td colSpan="3">
+                  <button id="add" type="submit">
+                    Save
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <th>Index</th>
+                <th>
+                  <button
+                    onClick={() => this.handleSort(this.state.books)}
+                    type="button"
+                    className="th"
+                  >
+                    {" "}
+                    Author
+                  </button>
+                </th>
+                <th>
+                  <button
+                    onClick={() => this.handleSortTitle(this.state.books)}
+                    type="button"
+                    className="th"
+                  >
+                    {" "}
+                    Title
+                  </button>
+                </th>
+                <th className="img-button">
+                  <img src={edit} alt="edit pencil" height="16" />
+                </th>
+                <th className="img-button">
+                  <img src={check} alt="check" height="16" />
+                </th>
+                <th className="img-button">
+                  <img src={del} alt="delete button" height="16" />
+                </th>
+              </tr>
+
+              <BookList
+                books={this.state.books}
+                edit={this.state.edit}
+                onCheck={this.onCheck}
+                onClickEdit={this.onClickEdit}
+                onDelete={this.onDelete}
+                onChange={this.onChange}
+                filteredBooks={this.filteredBooks}
+                search={this.state.search}
+              />
+            </tbody>
+          </table>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
