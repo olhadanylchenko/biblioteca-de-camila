22e87b0dc82c0b7a8a6bb8d9e9ffa727fece3620
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-07-18 21:47:09 +0200

    start of home screen, Invariant Violation if there are any books

--- native/components/Home.jsx
+++ native/components/Home.jsx
@@ -1,6 +1,79 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState, useContext, useEffect } from "react";
+import { Text, View, FlatList, StyleSheet } from "react-native";
+import { fetchFromApi } from "../utils/api";
 
-export default function Home() {
-  return <Text>HOME</Text>;
+import { UserContext } from "../contexts/UserContext";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  book: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+const Book = ({ title }) => (
+  <View style={styles.book}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+export default function Home({ navigation }) {
+  const [books, setBooks] = useState(null);
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    if (user) {
+      async function getBook() {
+        try {
+          const books = await fetchFromApi("/books", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+              Authorization: `Bearer ${user.token}`,
+            },
+            signal: abortController.signal,
+          });
+          setBooks(books);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      getBook();
+      return () => {
+        abortController.abort();
+      };
+    } else {
+      navigation.navigate("Login");
+    }
+  }, [user && user.token]);
+
+  const renderBook = ({ book }) => <Book title={book.title} />;
+
+  return (
+    <View style={styles.container}>
+      <Text>HOME</Text>
+      <FlatList
+        data={books}
+        renderBook={renderBook}
+        keyExtractor={(book) => book._id}
+      />
+    </View>
+  );
 }
